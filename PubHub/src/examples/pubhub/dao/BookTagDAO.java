897fb2433b5cb5eee8af03da8eb526408d549d8d
Christopher DiStasio <cadistasio@gmail.com>
2020-05-20 16:00:05 -0500

    Reorders method calls for consistency across program

--- PubHub/src/examples/pubhub/dao/BookTagDAO.java
+++ PubHub/src/examples/pubhub/dao/BookTagDAO.java
@@ -6,9 +6,9 @@ import examples.pubhub.model.BookTag;
 
 public interface BookTagDAO {
 	
-	public List<BookTag> addTagToBookByIsbn (String isbn, String tag_name);
-	public List<BookTag> deleteTagFromBookByIsbn (String isbn, String tag_name);
 	public List<BookTag> getAllTagsForGivenBook(String isbn);
 	public List<BookTag> getBooksWithGivenTag(String tag_name);
+	public List<BookTag> addTagToBookByIsbn (String isbn, String tag_name);
+	public List<BookTag> deleteTagFromBookByIsbn (String isbn, String tag_name);
 }
 

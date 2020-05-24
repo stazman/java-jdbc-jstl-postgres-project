69241c7a80e5904fa4ed83c22bdad02b08f5ee77
Christopher DiStasio <cadistasio@gmail.com>
2020-05-23 21:13:42 -0500

    Creates, stubs TestBookTagDAO class with getBooksWithGivenTag()

--- PubHub/src/examples/pubhub/dao/TestBookTagDAO.java
+++ PubHub/src/examples/pubhub/dao/TestBookTagDAO.java
@@ -8,17 +8,33 @@ public class TestBookTagDAO {
 
 	public static void main(String[] args) {
 		
-
-		BookTagDAO dao = new BookTagDAOImpl();
+		BookTagDAO btDao1 = new BookTagDAOImpl();
 		
-	    List<BookTag> list = dao.getAllTagsForGivenBook("1111111111111");
+	    List<BookTag> list1 = btDao1.getAllTagsForGivenBook("1111111111111", "fiction");
 	 
-	    for (int i = 0; i < list.size(); i++){
-	      BookTag t = list.get(i);
+	    for (int i = 0; i < list1.size(); i++){
+	      BookTag t = list1.get(i);
 	      System.out.println(t);
 	    }
+	    
+	    BookDAO bDao1 = new BookDAOImpl();
+		
+	    BookTagDAO btDao2 = new BookTagDAOImpl();
+		
+	    List<BookTag> list2 = btDao2.getBooksWithGivenTag("11111111111111", "fiction");
+	 
+	    for (int i = 0; i < list2.size(); i++){
+	      BookTag t = list2.get(i);
+	      System.out.println(t);
+	    }
+	    
+	    
 
 
 	}
+	
+	
+	
+	
 
 }

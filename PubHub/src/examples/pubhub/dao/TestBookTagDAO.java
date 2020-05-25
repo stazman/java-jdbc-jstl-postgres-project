9a359cd6154fd66172f7f9c531606999ee1eda07
Christopher DiStasio <cadistasio@gmail.com>
2020-05-25 18:55:02 -0500

    Test class prints deleteTagFromBookByIsbn() result

--- PubHub/src/examples/pubhub/dao/TestBookTagDAO.java
+++ PubHub/src/examples/pubhub/dao/TestBookTagDAO.java
@@ -1,40 +1,50 @@
 package examples.pubhub.dao;
 
-import java.util.List;
+//import java.util.List;
 
-import examples.pubhub.model.BookTag;
+//import examples.pubhub.model.BookTag;
 
 public class TestBookTagDAO {
-
+	
 	public static void main(String[] args) {
 		
-		BookTagDAO btDao1 = new BookTagDAOImpl();
-		
-	    List<BookTag> list1 = btDao1.getAllTagsForGivenBook("1111111111111", "fiction");
-	 
-	    for (int i = 0; i < list1.size(); i++){
-	      BookTag t = list1.get(i);
-	      System.out.println(t);
-	    }
+//		BookTagDAO btDao1 = new BookTagDAOImpl();
+//		
+//	    List<BookTag> list1 = btDao1.getAllTagsForGivenBook("1111111111111");
+//	 
+//	    for (int i = 0; i < list1.size(); i++){
+//	      BookTag t = list1.get(i);
+//	      System.out.println(t);
+//	    }
 	    
-	    BookDAO bDao1 = new BookDAOImpl();
+//		--------------------------------------------------------------------------------------------------------------------
 		
-	    BookTagDAO btDao2 = new BookTagDAOImpl();
+	    
+		//NOTE: The code for getBooksWithGivenTag() below is just a stub for now.
+	    
+//	    BookDAO bDao1 = new BookDAOImpl();
+//	    
+//	    //How to incorporate books based on BookTagDAOImpl ???
+//		
+//	    BookTagDAO btDao2 = new BookTagDAOImpl();
+//		
+//	    List<BookTag> list2 = btDao2.getBooksWithGivenTag("11111111111111", "fiction");
+//	 
+//	    for (int i = 0; i < list2.size(); i++){
+//	      BookTag t = list2.get(i);
+//	      System.out.println(t);
+//	    }
+	    
 		
-	    List<BookTag> list2 = btDao2.getBooksWithGivenTag("11111111111111", "fiction");
-	 
-	    for (int i = 0; i < list2.size(); i++){
-	      BookTag t = list2.get(i);
-	      System.out.println(t);
-	    }
-	    
-	    
+//		--------------------------------------------------------------------------------------------------------------------
 
-
+		
+	    BookTagDAO btDao4 = new BookTagDAOImpl();
+		
+	    boolean didDelete = btDao4.deleteTagFromBookByIsbn("3333333333333", "fiction");
+	 
+	    System.out.println(didDelete);
+	    
 	}
 	
-	
-	
-	
-
 }

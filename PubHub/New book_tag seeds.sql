2d99a9e74d19544b2ad76e20ae114cb333ac177e
Christopher DiStasio <cadistasio@gmail.com>
2020-05-25 15:56:48 -0500

    Adds new book_tag seeds

---
+++ PubHub/New book_tag seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO book_tags (isbn_13, tag_name) VALUES ('2222222222222', 'science fiction');
+INSERT INTO book_tags (isbn_13, tag_name) VALUES ('2222222222222', 'classic');
+INSERT INTO book_tags (isbn_13, tag_name) VALUES ('3333333333333', 'fiction');
+INSERT INTO book_tags (isbn_13, tag_name) VALUES ('3333333333333', 'classic');

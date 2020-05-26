f781775ab912be2919dacebb7d76e66e7a26f97f
Christopher DiStasio <cadistasio@gmail.com>
2020-05-26 13:10:05 -0500

    Stubs test class for addTagToBookByIsbn()

--- PubHub/src/examples/pubhub/dao/TestBookTagDAO.java
+++ PubHub/src/examples/pubhub/dao/TestBookTagDAO.java
@@ -1,22 +1,27 @@
 package examples.pubhub.dao;
 
-//import java.util.List;
+import java.util.List;
 
-//import examples.pubhub.model.BookTag;
+import examples.pubhub.model.BookTag;
 
 public class TestBookTagDAO {
 	
 	public static void main(String[] args) {
 		
-//		BookTagDAO btDao1 = new BookTagDAOImpl();
-//		
-//	    List<BookTag> list1 = btDao1.getAllTagsForGivenBook("1111111111111");
-//	 
-//	    for (int i = 0; i < list1.size(); i++){
-//	      BookTag t = list1.get(i);
-//	      System.out.println(t);
-//	    }
+		
+	/*	
+		BookTagDAO btDao1 = new BookTagDAOImpl();
+		
+	    List<BookTag> list1 = btDao1.getAllTagsForGivenBook("1111111111111");
+	 
+	    for (int i = 0; i < list1.size(); i++){
+	      BookTag t = list1.get(i);
+	      System.out.println(t);
+	    }
 	    
+	*/
+		
+	
 //		--------------------------------------------------------------------------------------------------------------------
 		
 	    
@@ -30,6 +35,13 @@ public class TestBookTagDAO {
 //		
 //	    List<BookTag> list2 = btDao2.getBooksWithGivenTag("11111111111111", "fiction");
 //	 
+//	   
+		
+		
+//		BookTagDAO btDao2 = new BookTagDAOImpl();
+//		
+//	    List<BookTag> list2 = btDao2.getBooksWithGivenTag("fiction");
+//	 
 //	    for (int i = 0; i < list2.size(); i++){
 //	      BookTag t = list2.get(i);
 //	      System.out.println(t);
@@ -39,11 +51,27 @@ public class TestBookTagDAO {
 //		--------------------------------------------------------------------------------------------------------------------
 
 		
-	    BookTagDAO btDao4 = new BookTagDAOImpl();
+		BookTagDAO btDao3 = new BookTagDAOImpl();
+//		
+	    List<BookTag> list3 = btDao3.addTagToBookByIsbn("3333333333333", "fiction"); //the parameter here only needs to be the "access column" that's passed into
+	    //the addTagToBookByIsbn method overall
+	    
+	    for (int i = 0; i < list3.size(); i++){
+	      BookTag t = list3.get(i);
+	      System.out.println(t);
+	    }
+
 		
-	    boolean didDelete = btDao4.deleteTagFromBookByIsbn("3333333333333", "fiction");
-	 
-	    System.out.println(didDelete);
+//		--------------------------------------------------------------------------------------------------------------------
+
+		
+		
+//	    BookTagDAO btDao4 = new BookTagDAOImpl();
+//		
+//	    boolean didDelete = btDao4.deleteTagFromBookByIsbn("3333333333333", "fiction");
+//	 
+//	    System.out.println(didDelete);
+	    
 	    
 	}
 	

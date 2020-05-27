b2c68fb4f629be9999272e0f9a721452faed5540
Christopher DiStasio <cadistasio@gmail.com>
2020-05-27 13:12:33 -0500

    Test class prints book ISBN, tag name for tag newly added to book

--- PubHub/src/examples/pubhub/dao/TestBookTagDAO.java
+++ PubHub/src/examples/pubhub/dao/TestBookTagDAO.java
@@ -24,7 +24,7 @@ public class TestBookTagDAO {
 	
 //		--------------------------------------------------------------------------------------------------------------------
 		
-	
+/*	
 		BookTagDAO btDao2 = new BookTagDAOImpl();
 		
 	    List<BookTag> list2 = btDao2.getBooksWithGivenTag("science fiction");
@@ -34,20 +34,20 @@ public class TestBookTagDAO {
 	      System.out.println(t);
 	    }
 	    
+*/
 		
 //		--------------------------------------------------------------------------------------------------------------------
 
 
-/*		BookTagDAO btDao3 = new BookTagDAOImpl();
+		BookTagDAO btDao3 = new BookTagDAOImpl();
 		
-	    List<BookTag> list3 = btDao3.addTagToBookByIsbn("3333333333333", "fiction"); //the parameter here only needs to be the "access column" that's passed into
-	    //the addTagToBookByIsbn method overall
+	    List<BookTag> list3 = btDao3.addTagToBookByIsbn("1111111111111", "cyberpunk");
 	    
 	    for (int i = 0; i < list3.size(); i++){
 	      BookTag t = list3.get(i);
 	      System.out.println(t);
 	    }
-*/
+
 	    
 		
 //		--------------------------------------------------------------------------------------------------------------------

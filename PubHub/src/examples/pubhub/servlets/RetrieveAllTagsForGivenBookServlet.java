7caa36e3b957de1a6ea67c35b8080e5b79676a2e
Christopher DiStasio <cadistasio@gmail.com>
2020-06-01 20:40:30 -0500

    Creates, stubs RetrieveAllTagsForGivenBookServlet

--- PubHub/src/examples/pubhub/servlets/RetrieveAllTagsForGivenBookServlet.java
+++ PubHub/src/examples/pubhub/servlets/RetrieveAllTagsForGivenBookServlet.java
@@ -1,11 +1,20 @@
 package examples.pubhub.servlets;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import examples.pubhub.dao.BookDAO;
+import examples.pubhub.dao.BookTagDAO;
+import examples.pubhub.dao.BookTagDAOImpl;
+import examples.pubhub.model.Book;
+import examples.pubhub.model.BookTag;
+import examples.pubhub.utilities.DAOUtilities;
 
 /**
  * Servlet implementation class RetrieveAllTagsForGivenBookServlet
@@ -26,16 +35,34 @@ public class RetrieveAllTagsForGivenBookServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		BookTagDAO dao = DAOUtilities.getBookTagDAO();	
+		List<BookTag> bookTagList = dao.getAllTagsForGivenBook(null);
+		
+		//How do I get the parameters needed to access isbn13
+		
+		request.getSession().setAttribute("book_tags", bookTagList);
+		
+		request.getRequestDispatcher("bookPublishingHome.jsp").forward(request, response);
+		
+		
+		
+		
+		
+		
+		
+		
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	
+	
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
 
 }

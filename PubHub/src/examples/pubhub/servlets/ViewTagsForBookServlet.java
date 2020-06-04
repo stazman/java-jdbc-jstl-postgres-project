b5ea86d079e07cc6d483a80cf1656fb8e8dbbdac
Christopher DiStasio <cadistasio@gmail.com>
2020-06-04 13:25:29 -0500

    Replaces ViewTagsForBook with ViewTagsForBookServlet

--- PubHub/src/examples/pubhub/servlets/ViewTagsForBookServlet.java
+++ PubHub/src/examples/pubhub/servlets/ViewTagsForBookServlet.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,17 +30,26 @@ import examples.pubhub.model.BookTag;
 import examples.pubhub.utilities.DAOUtilities;
 
 
-@MultipartConfig // This annotation tells the server that this servlet has
-// complex data other than forms
-//Notice the lack of the @WebServlet annotation? This servlet is mapped the old
-//fashioned way - Check the web.xml!
-public class ViewTagsForBook extends HttpServlet {
+@WebServlet("/ViewTagsForBook")
+public class ViewTagsForBookServlet extends HttpServlet {
 
 private static final long serialVersionUID = 1L;
 
 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	request.getRequestDispatcher("allTagsFromGivenBook.jsp").forward(request, response);
+	
+	String isbn13 = request.getParameter("isbn13");
+	
+	BookTagDAO dao = DAOUtilities.getBookTagDAO();
+	List<BookTag> bookTagList = dao.getAllTagsForGivenBook(isbn13);
+	
+	request.getSession().setAttribute("book_tags", bookTagList);
+	
+	request.getRequestDispatcher("allTagsForGivenBook.jsp").forward(request, response);
+	
 }
+
+}
+
 
 
 	///////////////////////////////////////////////////////////////////////////
@@ -69,5 +79,46 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response) t
 
 
 
-}
 
+
+//package examples.pubhub.servlets;
+//
+//import java.io.IOException;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+///**
+// * Servlet implementation class ViewTagsForBookServlet
+// */
+//@WebServlet("/ViewTagsForBookServlet")
+//public class ViewTagsForBookServlet extends HttpServlet {
+//	private static final long serialVersionUID = 1L;
+//       
+//    /**
+//     * @see HttpServlet#HttpServlet()
+//     */
+//    public ViewTagsForBookServlet() {
+//        super();
+//        // TODO Auto-generated constructor stub
+//    }
+//
+//	/**
+//	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//	}
+//
+//	/**
+//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+//
+//}
